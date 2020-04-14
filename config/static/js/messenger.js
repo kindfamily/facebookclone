abeb52308f9477a8f31b9d3c9a9aa0ebfebf53bb
kindfamily@kakao.com <kindfamily>
2020-04-14 16:08:18 +0000

    055 chat messenger

--- config/static/js/messenger.js
+++ config/static/js/messenger.js
@@ -1,98 +1,110 @@
 
-
-
-
-
 window.addEventListener('DOMContentLoaded',function(){
 
-
-
-
     (function(){
-
-
-
+        const inputUserId = document.querySelector('#user_id');
+        const inputUserName = document.querySelector('#user_name');
+        const inputRoomId = document.querySelector('#room_id');
         const userList = document.querySelector('#user_list');
         const timeLine = document.querySelector('#time_line');
-        const txt = document.querySelector('#txt');
-        const btn = document.querySelector('#btn');
+        const txtMessage = document.querySelector('#txt_message');
+        const btnSend = docuemnt.querySelector('#btn_send');
         const feed = document.querySelector('#feed');
-
-
+        
         userList.style.height = `${innerHeight - 42}px`;
         timeLine.style.height = `${innerHeight - 42}px`;
+        
+        const roomId = inputRoomId.value;
+        const userId = inputUserId.value;
+        const userName = inputUserName.value;
+        
+        // ws: //localhost:8000/ws/chat/1
+        var chatSocket = new WebSocket(
+            'ws://' + window.location.host + '/ws/chat/' + roomId + '/'
+        );
+        
+        chatSocket.onmessage = function(e) {
 
-
-
-
-
-
-        function sand(e){
-
-            e.preventDefault();
-
-            console.log(feed.scrollTop);
-            console.log(txt.value);
-
-
-            let template = `
             
-                <div>
-                   <div class="to">
-                       <div>${txt.value}</div>
-                   </div>
-                </div>
+            var _date = JSON.parse(e.data):
+            var message = _data.message;
+            var user_name = _data.user_name;
             
-            `;
-
-
-            feed.insertAdjacentHTML('beforeend',template);
-            txt.value = '';
-
+            appendMessageToFeed(_data);
+            
+            txtMessage.value= '';
+            
+            feed.scrollTop = feed.scrollHeight;
         }
-
-
-
-
-        btn.addEventListener('click',sand);
-        txt.addEventListener('keypress',function(e){
-            console.log(e.code);
-
-
-
-
-
-            if(e.code === 'Enter'){
-
-
-
-                let template = `
+        
+        function appendMessageToFeed(data){
+            const currentUserId = String(inputUserId.value);
+            const message = data.message;
+            const messageUserId = String(data.user_id);
             
-                <div>
-                   <div class="to">
-                       <div>${txt.value}</div>
-                   </div>
-                </div>
-            
-            `;
-
-
-                feed.insertAdjacentHTML('beforeend',template);
-                txt.value = '';
-
-
-                feed.scrollTop = feed.scrollHeight;
-
+            if(currentUserId === messageUserId) {
+                appendMyMessageToFeed(message);
+            } else {
+                appendFriendMessageToFeed(message);
             }
+        }
+        
+        function appendMyMessageToFeed(message) {
+            
+            let template = `
+                <div> 
+                    <div class="to">
+                        <div>${message}</div>
+                    </div>
+                </div>
+            `;
+            
+            feed.insertAdjacentHTML('beforeend', template);
+        }
+        
+        function appendFriendMessageToFeed(message) {
+            
+            let template = `
+                <div> 
+                    <div class="from">
+                        <div>${message}</div>
+                    </div>
+                </div>
+            `;
+            
+            feed.insertAdjacentHTML('beforeend', template);
+        }
+        
+        function sendMessage() {
+            var inputMessage = document.querySelector('#txt_message');
+            var message = inputMessage.value;
+            if(message){
+                chatSocket.send(JSON.stringify({
+                    'user_id': userId,
+                    'user_name': userName,
+                    'room_name': roomId,
+                    'message': message
+                }));
+                inputMessage.value = '';
+                inputMessage.focus();
+            }
+            
+        }
+        
+        
+        
+        
 
 
+
+        btnSend.addEventListener('click', sendMessage);
+        txtMessage.addEventListener('keypress', function(e){
+            if(e.code === 'Enter'){
+                sendMessage();
+            }
         })
-
-
-
-
-
-
-
+        
+        
+        
     })();
 });
